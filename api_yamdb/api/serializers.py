29c9df2770f1db1a768a1d6328c336faabdf1dbc
Stas Voskresensky <zababki@yandex.ru>
2022-11-08 17:05:45 +0300

    flake8

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -25,7 +25,7 @@ class UserSerializer(serializers.ModelSerializer):
         return value
 
     def validate_role(self, value):
-	res = value
+        res = value
         user = self.context['request'].user
         if (user.role == user.USER and not user.is_superuser):
             res = user.USER
