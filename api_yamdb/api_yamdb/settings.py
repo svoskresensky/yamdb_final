522ce69085e869ebe417a2cecfb2a001b2d92eac
Stas Voskresensky <zababki@yandex.ru>
2022-11-10 15:16:45 +0300

    to review

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -64,16 +64,23 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 
 # Database
+#DATABASES = {
+#    'default': {
+#        'ENGINE': os.getenv('DB_ENGINE', default="django.db.backends.postgresql"),
+#        'NAME': os.getenv('DB_NAME'),
+#        'USER': os.getenv('POSTGRES_USER'),
+#        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+#        'HOST': os.getenv('DB_HOST'),
+#        'PORT': os.getenv('DB_PORT')
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default="django.db.backends.postgresql"),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
